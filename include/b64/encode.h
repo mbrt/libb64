e16783c20d7b548f16b9e0abecc9b5dff7789843
Michele Bertasi <michele.bertasi@gmail.com>
2014-09-25 21:41:08 +0200

    added function and minimize allocation

--- include/b64/encode.h
+++ include/b64/encode.h
@@ -12,64 +12,101 @@ For details, see http://sourceforge.net/projects/libb64
 
 namespace base64
 {
-	extern "C" 
-	{
-		#include "cencode.h"
-	}
+    extern "C"
+    {
+        #include "cencode.h"
+    }
 
-	struct encoder
-	{
-		base64_encodestate _state;
-		int _buffersize;
+    class encoder
+    {
+    public:
+        encoder(int buffersize_in = BUFFERSIZE)
+            : _buffersize(buffersize_in), _code(0), _plaintext(0)
+        { }
 
-		encoder(int buffersize_in = BUFFERSIZE)
-		: _buffersize(buffersize_in)
-		{}
+        ~encoder()
+        {
+            if (_code) delete[] _code;
+            if (_plaintext) delete[] _plaintext;
+        }
 
-		int encode(char value_in)
-		{
-			return base64_encode_value(value_in);
-		}
+        encoder(const encoder& ) = delete;
+        encoder& operator= (const encoder& ) = delete;
 
-		int encode(const char* code_in, const int length_in, char* plaintext_out)
-		{
-			return base64_encode_block(code_in, length_in, plaintext_out, &_state);
-		}
+        int encode(char value_in)
+        {
+            return ::base64_encode_value(value_in);
+        }
 
-		int encode_end(char* plaintext_out)
-		{
-			return base64_encode_blockend(plaintext_out, &_state);
-		}
+        int encode(const char* code_in, const int length_in, char* plaintext_out)
+        {
+            return ::base64_encode_block(code_in, length_in, plaintext_out, &_state);
+        }
 
-		void encode(std::istream& istream_in, std::ostream& ostream_in)
-		{
-			base64_init_encodestate(&_state);
-			//
-			const int N = _buffersize;
-			char* plaintext = new char[N];
-			char* code = new char[2*N];
-			int plainlength;
-			int codelength;
+        int encode_end(char* plaintext_out)
+        {
+            return ::base64_encode_blockend(plaintext_out, &_state);
+        }
 
-			do
-			{
-				istream_in.read(plaintext, N);
-				plainlength = istream_in.gcount();
-				//
-				codelength = encode(plaintext, plainlength, code);
-				ostream_in.write(code, codelength);
-			}
-			while (istream_in.good() && plainlength > 0);
+        void encode(std::istream& istream_in, std::ostream& ostream_in)
+        {
+            ::base64_init_encodestate(&_state);
+            //
+            if (!_plaintext)
+                _plaintext = new char[_buffersize];
+            if (!_code)
+                _code = new char[2 * _buffersize];
+            int plainlength;
+            int codelength;
 
-			codelength = encode_end(code);
-			ostream_in.write(code, codelength);
-			//
-			base64_init_encodestate(&_state);
+            do
+            {
+                istream_in.read(_plaintext, _buffersize);
+                plainlength = istream_in.gcount();
+                //
+                codelength = encode(_plaintext, plainlength, _code);
+                ostream_in.write(_code, codelength);
+            }
+            while (istream_in.good() && plainlength > 0);
 
-			delete [] code;
-			delete [] plaintext;
-		}
-	};
+            codelength = encode_end(_code);
+            ostream_in.write(_code, codelength);
+            //
+            ::base64_init_encodestate(&_state);
+        }
+
+        void encode(const char* in, std::size_t len, std::ostream& ostream_in)
+        {
+            ::base64_init_encodestate(&_state);
+            //
+            if (!_code)
+                _code = new char[2 * _buffersize];
+            int plainlength;
+            int codelength;
+
+            do
+            {
+                plainlength = std::min(len, _buffersize);
+                //
+                codelength = encode(in, plainlength, _code);
+                ostream_in.write(_code, codelength);
+                in += plainlength;
+                len -= plainlength;
+            }
+            while (plainlength > 0);
+
+            codelength = encode_end(_code);
+            ostream_in.write(_code, codelength);
+            //
+            ::base64_init_encodestate(&_state);
+        }
+
+    private:
+        ::base64_encodestate _state;
+        const int _buffersize;
+        const char* _code;
+        const char* _plaintext;
+    };
 
 } // namespace base64
 
